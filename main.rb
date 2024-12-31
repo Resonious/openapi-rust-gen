0711f8f9819127d6b727640310aa3f2d089b663e
Nigel Baillie <nigel@baillie.dev>
2024-12-31 09:38:47 +0900

    try clone

--- main.rb
+++ main.rb
@@ -287,7 +287,7 @@ class OpenApiRustGenerator
     # Create structs for all components.
     @schema.fetch(:components).fetch(:schemas).each do |model, definition|
       if all_of = definition[:allOf]
-        o.puts "#[derive(Serialize, Deserialize, Debug)]"
+        o.puts "#[derive(Clone, Serialize, Deserialize, Debug)]"
         o.puts "pub struct #{model} {"
         all_of.each { |d| puts_struct_fields(o, d, model) }
         o.puts "}"
@@ -296,7 +296,7 @@ class OpenApiRustGenerator
 
       case definition.fetch(:type)
       when "object"
-        o.puts "#[derive(Serialize, Deserialize, Debug)]"
+        o.puts "#[derive(Clone, Serialize, Deserialize, Debug)]"
         o.puts "pub struct #{model} {"
         puts_struct_fields(o, definition, model)
         o.puts "}"
@@ -553,7 +553,7 @@ class OpenApiRustGenerator
         o.puts
 
         if definition[:type] == "object"
-          o.puts "#[derive(Serialize, Deserialize, Debug)]"
+          o.puts "#[derive(Clone, Serialize, Deserialize, Debug)]"
           o.puts "pub struct #{type_name} {"
           puts_struct_fields(o, definition, type_name)
           o.puts "}"
@@ -562,7 +562,7 @@ class OpenApiRustGenerator
           similar_enums[enum_key] ||= []
           similar_enums[enum_key] << [type_name, definition]
 
-          o.puts "#[derive(Serialize, Deserialize, Debug)]"
+          o.puts "#[derive(Clone, Serialize, Deserialize, Debug)]"
           o.puts "pub enum #{type_name} {"
           enum.each do |item|
             o.puts "    #[serde(rename = #{item.to_s.inspect})]"
